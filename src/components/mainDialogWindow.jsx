1f7809dec454ce1c516d6ef8ea4d34f7477ebafa
chrnk-exe <ivan_kot2001@mail.ru>
2021-10-25 23:06:23 +0300

    textarea

--- src/components/mainDialogWindow.jsx
+++ src/components/mainDialogWindow.jsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import classes from './styles/MainWindow.module.css'
 import MessageList from './messageList'
 
 const MainWindow = (props) => {
+    const textareaRef = useRef(null);
     const [messageText, setMessageText] = useState('')
 
     const sendMessage = (e) => {
         e.preventDefault()
         console.log(messageText)
+        setMessageText('');
     }
 
     /* Dev test items */
@@ -15,6 +17,11 @@ const MainWindow = (props) => {
 
 
     /* Dev test items */
+    useEffect(()=> {
+        textareaRef.current.style.height = "0px";
+        const scrollHeight = textareaRef.current.scrollHeight;
+        textareaRef.current.style.height = scrollHeight + "px";
+    }, [messageText])
 
     return (
         <div className={classes.dialogWindow}> 
@@ -24,10 +31,9 @@ const MainWindow = (props) => {
             </div>
             <form className={classes.txtArea}>
             <div className={classes.wInputContainer}>
-                <div className={classes.wInputTextGroup}>
-                    <div className={classes.wInputText} contentEditable={true} onInput={(e) => setMessageText(e.currentTarget.textContent)}></div>
-                    <div className={classes.wPlaceholder}>Type a message</div>
-                </div>
+                    {/*<div className={classes.wInputText} contentEditable={true} onInput={(e) => setMessageText(e.currentTarget.textContent)}></div>
+                    <div className={classes.wPlaceholder}>Type a message</div>*/}
+                    <textarea ref={textareaRef} className={classes.wrapper} placeholder="Type a message" value={messageText} onChange={(e) => (setMessageText(e.target.value))}></textarea>
             </div>
             <button onClick={sendMessage}>Send</button>
             </form>
