b25e5a361b7068767f70b290147622ba8993e106
chrnk-exe <ivan_kot2001@mail.ru>
2021-11-28 16:16:18 +0300

    rangeStyled

--- src/components/messenger/dialogs/Settings/UISettings/UISettings.jsx
+++ src/components/messenger/dialogs/Settings/UISettings/UISettings.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import classes from './UISettings.module.css'
 
 const UISettings = () => {
+    const [font, setFont] = useState(16)
+
     return (
-        <div>
-            UI settingss
+        <div className={classes.uisettings}>
+            <input type='number' readOnly value={font}/><input className={classes.fontSizer} type='range' max={24} min={10} step={2} value={font} onChange={(e) => {setFont(e.target.value)}}/>
         </div>
     )
 }
