66795f13cdf821180b1331f8b754dea37be5a212
ASUS <anonimus.pochti@gmail.com>
2021-12-04 15:40:06 +0300

    SOCKETS talk dialog search

--- sweater/routes.py
+++ sweater/routes.py
@@ -291,6 +291,7 @@ def login():
                                         "id": user.id,
                                         "name": user.name,
                                         "dialogs": response_list,
+                                        "avatar_id": user.avatar_id,
                                         "info": "authorization successful"})
                     else:
                         return jsonify({"status": 1, "info": "email not activated"})
@@ -429,7 +430,11 @@ def search_user():
             users = db.session.query(User).filter(User.name.startswith(value)).filter(
                 User.id.not_in(own_members)).limit(10).all()
 
-            response = list({"id": user.id, "name": user.name} for user in users)
+            response = list({"id": user.id,
+                             "name": user.name,
+                             "avatar_id": user.avatar_id,
+                             "user_status": user.user_status} for user in users)
+
             return jsonify({"status": 0, "users": response})
         except Exception as e:
             return jsonify({"status": 666, "info": str(e) + traceback.format_exc()})
@@ -444,8 +449,9 @@ def add_to_json(obj, count):
 @app.route('/create_dialog', methods=['POST'])
 def create_dialog():
     if request.method == "POST":
-
         try:
+            global rooms_list
+
             data = request.get_json()
             title = data["title"]
             members = data["members"]
@@ -472,8 +478,28 @@ def create_dialog():
             for user_id in members:
                 user = db.session.query(User).filter_by(id=user_id).first_or_404()
                 user.dialogs = add_to_json(user.dialogs, dialog.id)
-
             db.session.commit()
+
+            dialog_members = json.loads(dialog.members)
+
+            for member_id in dialog_members:
+                if member_id != cur_id and str(member_id) in rooms_list:
+
+                    members_list = []
+                    other_members = dialog_members.copy()
+                    other_members.remove(member_id)
+
+                    for other_member in other_members:
+                        member = db.session.query(User).filter_by(id=other_member).first_or_404()
+
+                        members_list.append({"name": member.name,
+                                             "user_status": member.user_status,
+                                             "avatar_id": member.avatar_id})
+
+                    emit('new_dialog', {'info': 'new_dialog',
+                                        'dialog_id': dialog.id,
+                                        'other_members': other_members},
+                         to=str(member_id), namespace='/')
 
             return jsonify({"status": 0, "id": dialog.id})
 
@@ -487,8 +513,8 @@ def create_talk():
         try:
             data = request.get_json()
             title = data["title"]
-            # members = data["members"]
             dialog_id = data["dialog_id"]
+            cur_id = int(current_user.get_id())
 
             talk = Talk(title=title, date_create=str(datetime.datetime.utcnow() + datetime.timedelta(hours=3)))
             db.session.add(talk)
@@ -496,8 +522,18 @@ def create_talk():
 
             dialog = db.session.query(Dialog).filter_by(id=dialog_id).first_or_404()
             dialog.talks = add_to_json(dialog.talks, talk.id)
-
             db.session.commit()
+
+            dialog_members = json.loads(dialog.members)
+            other_members = dialog_members.copy()
+            other_members.remove(cur_id)
+
+            for member_id in other_members:
+                if str(member_id) in rooms_list:
+
+                    emit('new_talk', {"dialog_id": dialog.id,
+                                      "talk_id": talk.id,
+                                      "title": talk.title})
 
             return jsonify({"status": 0, "id": talk.id})
 
